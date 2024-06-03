b6ffa040ebc13029f4e7b374dbe30dee8c80a262
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-06-03 20:46:18 -0300

    primeiro commit

---
+++ release.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    branches: ['main'],
+    repositoryUrl: 'https://github.com/your-username/your-repo.git',
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/github',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['package.json', 'CHANGELOG.md'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+    ],
+  };
+  
