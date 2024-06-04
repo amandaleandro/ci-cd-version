02135bba17e25dc7da6356d3e5ee94184ac1b60e
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-06-04 02:52:56 -0300

    fix:corrigindo um bug pelo amor de Deus funciona

--- increment_version.sh
+++ increment_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Obtém a versão mais recente da tag
-latest_tag=$(git describe --tags --abbrev=0)
+latest_tag=$(git describe --tags --abbrev=0 2>/dev/null)
 
 # Verifica se a tag mais recente existe
 if [ -z "$latest_tag" ]; then
@@ -13,26 +13,20 @@ else
   minor=$(echo "$latest_tag" | cut -d. -f2)
   patch=$(echo "$latest_tag" | cut -d. -f3)
 
-  # Verifica se houve commits desde a última tag
-  if git log "$latest_tag"..HEAD --oneline | grep -qE "feat|fix|perf"; then
-    # Incrementa a versão de acordo com o versionamento semântico
-    if [ "$major" -eq "0" ]; then
-      # Se a versão principal for 0, incrementamos a versão secundária
-      minor=$((minor + 1))
-    else
-      # Caso contrário, incrementamos a versão principal
-      major=$((major + 1))
-    fi
+  # Incrementa a versão com base nas mensagens de commit
+  if git log --format=%B -n 1 HEAD | grep -q '\[major\]'; then
+    major=$((major + 1))
+    minor=0
+    patch=0
+  elif git log --format=%B -n 1 HEAD | grep -q '\[minor\]'; then
+    minor=$((minor + 1))
+    patch=0
+  else
+    patch=$((patch + 1))
   fi
 
-  # Verifica se a nova versão é maior ou igual a 1.0.0
-  if [ "$major" -eq "1" ] && [ "$minor" -eq "0" ] && [ "$patch" -eq "0" ]; then
-    # Se a nova versão for menor que 1.0.0, definimos como 1.0.0
-    new_version="1.0.0"
-  else
-    # Cria a nova versão
-    new_version="$major.$minor.$patch"
-  fi
+  # Cria a nova versão
+  new_version="$major.$minor.$patch"
 fi
 
 echo "Nova versão: $new_version"
